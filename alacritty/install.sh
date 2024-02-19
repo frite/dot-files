f8e998c29a2e73ea34053b59b24eb6f6a8d10b44
Manolis <mmavrofides@gmail.com>
2024-02-19 23:11:29 +0200

    Introduce Alacritty setup/script

---
+++ alacritty/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/theme
+cp alacritty.toml ~/.config/alacritty/alacritty.toml
